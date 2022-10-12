796a5c51fa0cf11862755ed5aff5b6986253d723
xlam0004 <113871296+xlam0004@users.noreply.github.com>
2022-10-13 02:31:14 +0800

    Add files via upload

--- vega-lite-vis.js
+++ vega-lite-vis.js
@@ -1,23 +1,23 @@
 var vg_1 = "prop_symbol_map.vg.json";
-vegaEmbed("#propotional_symbol_map", vg_1).then(function(result){}).catch(console.error);
+vegaEmbed("#propotional_symbol_map", vg_1, {"actions": false}).then(function(result){}).catch(console.error);
 
 var vg_2 = "bump_chart.vg.json";
-vegaEmbed("#bump_chart", vg_2).then(function(result){}).catch(console.error);
+vegaEmbed("#bump_chart", vg_2, {"actions": false}).then(function(result){}).catch(console.error);
 
 var vg_3 = "diverging_stack_bar_chart.vg.json";
-vegaEmbed("#diverging_stack_bar_chart", vg_3).then(function(result){}).catch(console.error);
+vegaEmbed("#diverging_stack_bar_chart", vg_3, {"actions": false}).then(function(result){}).catch(console.error);
 
 var vg_4 = "box_plot.vg.json";
-vegaEmbed("#box_plot", vg_4).then(function(result){}).catch(console.error);
+vegaEmbed("#box_plot", vg_4, {"actions": false}).then(function(result){}).catch(console.error);
 
 var vg_5 = "donut_chart_map.vg.json";
-vegaEmbed("#donut_chart_map", vg_5).then(function(result){}).catch(console.error);
+vegaEmbed("#donut_chart_map", vg_5, {"actions": false}).then(function(result){}).catch(console.error);
 
 var vg_6 = "mosaic_chart.vg.json";
-vegaEmbed("#mosaic_chart", vg_6).then(function(result){}).catch(console.error);
+vegaEmbed("#mosaic_chart", vg_6, {"actions": false}).then(function(result){}).catch(console.error);
 
-var vg_7 = "bubble_plot.vg.json";
-vegaEmbed("#bubble_plot", vg_7).then(function(result){}).catch(console.error);
+var vg_7 = "bubble_plot_brush.vg.json";
+vegaEmbed("#bubble_plot", vg_7, {"actions": false}).then(function(result){}).catch(console.error);
 
 var vg_8 = "scatter_plot_annotated.vg.json";
-vegaEmbed("#scatter_plot", vg_8).then(function(result){}).catch(console.error);
+vegaEmbed("#scatter_plot", vg_8, {"actions": false}).then(function(result){}).catch(console.error);
