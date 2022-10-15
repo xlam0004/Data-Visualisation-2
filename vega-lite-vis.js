70ddf360456a4a014400c9c78cf70aa7b889c42c
xlam0004 <113871296+xlam0004@users.noreply.github.com>
2022-10-15 17:27:58 +0800

    Add files via upload

--- vega-lite-vis.js
+++ vega-lite-vis.js
@@ -10,11 +10,11 @@ vegaEmbed("#diverging_stack_bar_chart", vg_3, {"actions": false}).then(function(
 var vg_4 = "box_plot.vg.json";
 vegaEmbed("#box_plot", vg_4, {"actions": false}).then(function(result){}).catch(console.error);
 
-var vg_5 = "donut_chart_map.vg.json";
-vegaEmbed("#donut_chart_map", vg_5, {"actions": false}).then(function(result){}).catch(console.error);
+var vg_5 = "donut_chart_concat.vg.json";
+vegaEmbed("#donut_chart_concat", vg_5, {"actions": false}).then(function(result){}).catch(console.error);
 
-var vg_6 = "mosaic_chart.vg.json";
-vegaEmbed("#mosaic_chart", vg_6, {"actions": false}).then(function(result){}).catch(console.error);
+var vg_6 = "range_dot_plot.vg.json";
+vegaEmbed("#range_dot_plot", vg_6, {"actions": false}).then(function(result){}).catch(console.error);
 
 var vg_7 = "bubble_plot_brush.vg.json";
 vegaEmbed("#bubble_plot", vg_7, {"actions": false}).then(function(result){}).catch(console.error);
